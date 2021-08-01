4fca00abc8f76d18a3de2da4b8f21f102782cc7d
Luiz Almeida (lhas) <luizhrqas@gmail.com>
2021-08-01 13:38:42 -0300

    initialize table mouseover effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-table-hover';
+  hover = 0;
+
+  items = [
+    {
+      id: 1,
+      name: 'Luiz',
+      role: 'Desenvolvedor Front-end',
+      phone: '(51) 99103-1355',
+    },
+    {
+      id: 2,
+      name: 'Jeff',
+      role: 'Desenvolvedor Front-end',
+      phone: '(51) 99103-1355',
+    },
+    {
+      id: 3,
+      name: 'João',
+      role: 'Desenvolvedor Front-end',
+      phone: '(51) 99103-1355',
+    },
+  ];
+
+  mouseOver(item: any) {
+    this.hover = item.id;
+  }
+
+  mouseOut(item: any) {
+    if (item.id === this.hover) {
+      this.hover = 0;
+    }
+  }
 }
